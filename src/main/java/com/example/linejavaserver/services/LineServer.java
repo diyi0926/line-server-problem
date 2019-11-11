bfffdf89635df398e818564c63ea8e26a7f0604c
yidibj <yi.di@husky.neu.edu>
2019-11-11 16:43:38 -0500

    add plugin to fix spring getFile problem inside jar

--- src/main/java/com/example/linejavaserver/services/LineServer.java
+++ src/main/java/com/example/linejavaserver/services/LineServer.java
@@ -1,22 +1,34 @@
 package com.example.linejavaserver.services;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
+import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class LineServer {
     private static LineServer instance = null;
     private List<Long> indexes = new ArrayList<>();
     private LineServer() {
-        long curIndex = 0;
-        indexes.add(curIndex);
-        InputStream inputStream = this.getClass().getResourceAsStream("/file.txt");
-        Scanner sc = new Scanner(inputStream, "UTF-8");
-        while (sc.hasNextLine()) {
-            curIndex += sc.nextLine().length() + 2; // length of next line and '/n'
+        try{
+            long curIndex = 0;
             indexes.add(curIndex);
+            InputStream inputStream = new ClassPathResource("file.txt").getInputStream();
+            Scanner sc = new Scanner(inputStream, "UTF-8");
+            while (sc.hasNextLine()) {
+                curIndex += sc.nextLine().length() + 2; // length of next line and '/n'
+                indexes.add(curIndex);
+            }
+            sc.close();
+        } catch(IOException e) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
         }
-        sc.close();
+        
     }
     public static LineServer getInstance() {
         if (instance == null) instance = new LineServer();
